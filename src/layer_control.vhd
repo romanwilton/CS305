9270713158f34fe1bbc8da7e3c607d466c866f4d
Hamish O'Neill <hone075@aucklanduni.ac.nz>
2017-04-25 12:48:19 +1200

    Add architecture for layer control.

--- src/layer_control.vhd
+++ src/layer_control.vhd
@@ -8,3 +8,19 @@ entity layer_control is
 		RGB_out	: OUT std_logic_vector(2 downto 0)
 	);
 end entity layer_control;
+
+architecture arch of layer_control is
+begin
+	process (layers) is
+	begin
+		if (layers(8 downto 6) /= "000") then
+			RGB_out <= layers(8 downto 6);
+		elsif (layers(5 downto 3) /= "000") then
+			RGB_out <= layers(5 downto 3);
+		elsif (layers(2 downto 0) /= "000") then
+			RGB_out <= layers(2 downto 0);
+		else
+			RGB_out <= "000";
+		end if;
+	end process;
+end architecture arch;
