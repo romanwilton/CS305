23ceb31dcb9d7c3bd994173b7885056063704747
Roman Amor <romanwilton@gmail.com>
2017-04-25 14:48:30 +1200

    Added clock dividers to make movement noticeable

--- src/ai_tank.vhd
+++ src/ai_tank.vhd
@@ -13,8 +13,9 @@ entity ai_tank is
 end entity ai_tank;
 
 architecture arch of ai_tank is
-	signal x : std_logic_vector(9 downto 0);
+	signal x : std_logic_vector(9 downto 0) := "0000000000";
 	signal y : std_logic_vector(9 downto 0) := std_logic_vector(to_unsigned(80, 10));
+	signal newClk : std_logic := '0';
 	component draw_object is
 		port (
 			pixel_row, pixel_col, x, y : IN std_logic_vector(9 downto 0);
@@ -24,9 +25,21 @@ architecture arch of ai_tank is
 begin
 	output_drawing : draw_object port map(pixel_row, pixel_col, x, y, RGB_out);
 	collision <= '0';
-	clockDriven : process( clock )
+
+clockDiv : process( clock )
+	variable counter : std_logic_vector(16 downto 0) := "00000000000000000";
+begin
+	if(rising_edge(clock)) then
+		if(counter = "11111111111111111") then
+			newClk <= NOT newClk;
+		end if;
+		counter := counter +1;
+	end if;
+end process ; -- clockDiv
+
+	clockDriven : process( newClk )
 	begin
-		if(rising_edge(clock)) then
+		if(rising_edge(newClk)) then
 			if(x < std_logic_vector(to_unsigned(640, 10))) then
 				x <= x+1;
 			end if;
