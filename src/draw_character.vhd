4fae1a40bd6560503782a985d14ff7ba4c33b318
Hamish O'Neill <hone075@aucklanduni.ac.nz>
2017-04-30 14:25:33 +1200

    Show both digits of the score on the screen, using a helper component.

---
+++ src/draw_character.vhd
@@ -0,0 +1,44 @@
+LIBRARY IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+use IEEE.numeric_std.all;
+
+entity draw_character is
+	generic (
+		x, y : in natural
+	);
+	port (
+		digit : in std_logic_vector(3 downto 0);
+		pixel_row, pixel_col : in std_logic_vector(9 downto 0);
+		enable : out std_logic;
+		character_address : OUT STD_LOGIC_VECTOR (5 DOWNTO 0);
+		font_row, font_col : OUT STD_LOGIC_VECTOR (2 DOWNTO 0)
+	);
+end entity draw_character;
+
+architecture arch of draw_character is
+begin
+	
+	process (pixel_col, pixel_row, digit) is
+		variable col_v, row_v : STD_LOGIC_VECTOR(9 DOWNTO 0);
+	begin
+		
+		enable <= '0';
+		character_address <= "000000";
+		font_row <= "000";
+		font_col <= "000";
+	
+		if (pixel_col >= x and pixel_col < (x + 8)) then
+			if (pixel_row >= y and pixel_row < (y + 8)) then
+				character_address <= O"60" + digit;
+				col_v := pixel_col - x;
+				font_col <= col_v(2 downto 0);
+				row_v := pixel_row - y;
+				font_row <= row_v(2 downto 0);
+				enable <= '1';
+			end if;
+		end if;
+	
+	end process;
+	
+end architecture arch;
--- src/draw_score.vhd
+++ src/draw_score.vhd
@@ -23,33 +23,61 @@ architecture arch of draw_score is
 		);
 	end component char_rom;
 	
-	signal character_address : STD_LOGIC_VECTOR (5 DOWNTO 0);
-	signal row, col : STD_LOGIC_VECTOR (2 DOWNTO 0);
-	signal rom_out : std_logic;
+	component draw_character is
+		generic (
+			x, y : in natural
+		);
+		port (
+			digit : in std_logic_vector(3 downto 0);
+			pixel_row, pixel_col : in std_logic_vector(9 downto 0);
+			enable : out std_logic;
+			character_address : OUT STD_LOGIC_VECTOR (5 DOWNTO 0);
+			font_row, font_col : OUT STD_LOGIC_VECTOR (2 DOWNTO 0)
+		);
+	end component draw_character;
+	
+	signal character_address, ca1, ca2 : STD_LOGIC_VECTOR (5 DOWNTO 0);
+	signal row, col, row1, col1, row2, col2: STD_LOGIC_VECTOR (2 DOWNTO 0);
+	signal rom_out, en1, en2 : std_logic;
 
 begin
 
-	D: char_rom port map (character_address, row, col, clk, rom_out);
+	CHARACTER_ROM: char_rom port map (character_address, row, col, clk, rom_out);
 	
-	process (pixel_col, pixel_row, digit1, rom_out) is
-		variable col_v, row_v : STD_LOGIC_VECTOR(9 DOWNTO 0);
+	DIGIT1_DRAW : draw_character generic map (592, 10) 
+		port map (digit1, pixel_row, pixel_col, en1, ca1, row1, col1);
+	DIGIT2_DRAW : draw_character generic map (582, 10) 
+		port map (digit2, pixel_row, pixel_col, en2, ca2, row2, col2);
+	
+	process (en1, en2, ca1, ca2, row1, row2, col1, col2) is
 	begin
-		
-		colour_out <= X"0000";
 	
-		if (pixel_col >= 572 and pixel_col < 580) then
-			if (pixel_row >= 10 and pixel_row < 18) then
-				character_address <= O"60" + digit1;
-				col_v := pixel_col - 572;
-				col <= col_v(2 downto 0);
-				row_v := pixel_row - 10;
-				row <= row_v(2 downto 0);
-				if (rom_out = '1') then
-					colour_out <= "1000000000011111";
-				end if;
-			end if;
+		character_address <= "000000";
+		row <= "000";
+		col <= "000";
+	
+		if en1 = '1' then
+			character_address <= ca1;
+			row <= row1;
+			col <= col1;
+		end if;
+		
+		if en2 = '1' then
+			character_address <= ca2;
+			row <= row2;
+			col <= col2;
 		end if;
 	
+	end process;
+	
+	process (rom_out, en1, en2) is
+	begin
+		colour_out <= X"0000";
+		if (en1 = '1' or en2 = '1') then
+			if (rom_out = '1') then
+				colour_out <= "1000000000011111";
+			end if;
+		end if;
 	end process;
 	
 end architecture arch;
