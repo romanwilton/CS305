67b7a5e23c667df44b75ea1db5487842f56a590d
Roman Amor <romanwilton@gmail.com>
2017-04-30 18:02:27 +1200

    Fix image rendering to be how WE think is correct. Tank still has problems

--- src/draw_object.vhd
+++ src/draw_object.vhd
@@ -39,7 +39,7 @@ begin
 	process (pixel_row, pixel_col, x, y, RGB) is
 	begin
 		RGB_out <= X"0000";
-		if (x < pixel_col + width/2) and (x + width/2 >= pixel_col) then
+		if (x <= pixel_col + width/2) and (x + width/2 > pixel_col) then
 			if (y <= pixel_row + height/2) and (y + height/2 > pixel_row) then
 				RGB_out <= RGB;
 			end if;
